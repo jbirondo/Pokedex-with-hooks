93213ac4c2d46fcb8cf3b66506ce1335c15cf9db
Eugene Birondo <jbirondo@gmail.com>
2021-01-20 23:40:21 -0800

    Working on Search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import PokeShow from './PokeShow'
+import Search from './Search'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Search/>
       <PokeShow/>
     </div>
   );
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search(){
+    return (
+        <input type="search" placeholder="Type a name or a pokemon number">
+        </input>
+        
+    )
+}
+
+export default Search
