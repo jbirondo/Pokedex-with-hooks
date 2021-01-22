09d86c26745219520b51cf22fb04b739c8f8e87b
Eugene Birondo <jbirondo@gmail.com>
2021-01-22 00:08:44 -0800

    Working on positioning divs

--- src/PokeShow.js
+++ src/PokeShow.js
@@ -4,46 +4,16 @@ import './PokeShow.css'
 function PokeShow(){
     return (
         <div className="pokeShowContainer">
-            <div 
-                className="pokeName">
-                506 Lillipup
-            </div>
-            <div
-                className="pokeType">
-                Type(s) :  Normal
-            </div>
-            <div
-                className="pokeWT">
-                WT : 41
-            </div>
-            <div
-                className="pokeHT">
-                HT : 4
-            </div>
-            <div
-                className="pokeHP">
-                HP : 45
-            </div>
-            <div
-                className="pokeATK">
-                ATK : 60
-            </div>
-            <div
-                className="pokeDEF">
-                DEF : 45
-            </div>
-            <div
-                className="pokeSPD">
-                SPD : 55
-            </div>
-            <div
-                className="pokeSATK">
-                SpecATK : 25
-            </div>
-            <div
-                className="pokeSDEF">
-                SpecDEF : 45
-            </div>
+            <div className="pokeName">506 Lillipup</div>
+            <div className="pokeType">Type(s) :  Normal</div>
+            <div className="pokeWT">WT : 41</div>
+            <div className="pokeHT">HT : 4</div>
+            <div className="pokeHP">HP : 45</div>
+            <div className="pokeATK">ATK : 60</div>
+            <div className="pokeDEF">DEF : 45</div>
+            <div className="pokeSPD">SPD : 55</div>
+            <div className="pokeSATK">SpecATK : 25</div>
+            <div className="pokeSDEF">SpecDEF : 45</div>
             <img 
                 className="pokeImg" 
                 src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/506.png'
