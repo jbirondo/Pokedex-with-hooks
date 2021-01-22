9cf3c1d75d83b6f06e13220bd32047a2874a3cc9
Eugene Birondo <jbirondo@gmail.com>
2021-01-21 22:39:16 -0800

    Working on positioning divs

--- src/PokeShow.js
+++ src/PokeShow.js
@@ -3,7 +3,15 @@ import './PokeShow.css'
 
 function PokeShow(){
     return (
-        <div className="pokeShowContainer"></div>
+        <div className="pokeShowContainer">
+            <div 
+                className="pokeName">
+            </div>
+            <img 
+                className="pokeImg" 
+                src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/506.png'>
+            </img>
+        </div>
     )
 }
 
